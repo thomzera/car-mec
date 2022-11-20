a43a9ea236b4a9d950f81d2bca10ee4e43a9fb5a
thomzera <93238590+thomzera@users.noreply.github.com>
2022-11-20 12:18:50 -0300

    feat: add login, logout e registro de usuários

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
+
+    # LOGIN LOGOUT REGISTER
+    path("login/", views.Login.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("registrar/", views.RegistroUsuario.as_view(), name="registrar"),
 
     # CRUD CARROS
     path("carros/", views.ListaCarros.as_view(), name="carros"),
--- project/core/views.py
+++ project/core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import (CreateView, DeleteView, FormView,
@@ -11,6 +14,35 @@ from . import models
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+# ########## LOGIN & REGISTER ###########
+
+
+class Login(LoginView):
+    template_name = 'core/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+class RegistroUsuario(FormView):
+    template_name = 'core/registrar.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegistroUsuario, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('index')
+        return super(RegistroUsuario, self).get(*args, **kwargs)
 
 
 # ########## CRUD CARRO ###########
