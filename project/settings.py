2f1b658d6cfea8cc3b0a2fe7f69eee56c4f39ec3
thomzera <93238590+thomzera@users.noreply.github.com>
2022-11-27 21:13:35 -0300

    feat: staticfiles config

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = "static/"
 
 LOGIN_URL = "login"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "project"/  "core" / "assets"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
