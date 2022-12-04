2deef85e0b09ca484b48fae8b8586eefb1227133
thomzera <93238590+thomzera@users.noreply.github.com>
2022-12-03 21:04:34 -0300

    feat: opcao de fazer login com email ou usuario

--- project/settings.py
+++ project/settings.py
@@ -132,6 +132,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "project"/  "core" / "assets"
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'project.core.backends.EmailOrUsernameBackend',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
